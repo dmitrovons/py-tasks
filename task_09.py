1d86d05cf9ef01336f74aa0fb1b38b050c51d8f8
dmitrovons <lezikxlvl@gmail.com>
2022-04-04 19:34:11 +0200

    Lesson 2

--- task_09.py
+++ task_09.py
@@ -7,3 +7,18 @@ conditions are:
 9, 10, 11 - autumn
 12, 1, 2 - winter
 '''
+
+def Method_1(aSpring, aSummer, aAutumn, aWinter ):
+    pass
+        
+    
+
+
+
+
+Spring = [3,4,5]
+Summer = [6,7,8]
+Autumn = [9,10,11]
+Winter = [12,1,2]
+
+Method_1(Spring, Summer, Autumn, Winter)
