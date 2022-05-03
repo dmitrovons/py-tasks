0320796f4caa5b7ecb0e2e9d477edcbd4e25f631
dmitrovons <lezikxlvl@gmail.com>
2022-05-03 21:03:59 +0200

    test

---
+++ Dmitro/Task_01.py
@@ -0,0 +1,11 @@
+'''
+Show first three not even digits from a list
+[12, 6, 2, 3, 15, 8, 13, 10, 7]
+'''
+
+def Method_1(aArr):
+    return ('Hello', sorted(aArr))
+
+
+Arr = [12, 6, 2, 3, 15, 8, 13, 10, 7]
+print(Method_1(Arr))
